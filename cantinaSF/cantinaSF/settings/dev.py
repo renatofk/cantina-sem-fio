d94a4225a927950a934a9ee4b57ae53c7572e6dc
Renato Foster <renatofk@gmail.com>
2025-06-17 17:12:21 -0300

    remove variables env

--- cantinaSF/cantinaSF/settings/dev.py
+++ cantinaSF/cantinaSF/settings/dev.py
@@ -11,6 +11,17 @@ ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", ""),
+        "USER": os.getenv("DB_USER", ""),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", ""),
+        "PORT": os.getenv("DB_PORT", ""),
+    }
+}
+
 
 try:
     from .local import *
--- cantinaSF/cantinaSF/settings/production.py
+++ cantinaSF/cantinaSF/settings/production.py
@@ -2,6 +2,17 @@ from .base import *
 
 DEBUG = False
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", ""),
+        "USER": os.getenv("DB_USER", ""),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", ""),
+        "PORT": os.getenv("DB_PORT", ""),
+    }
+}
+
 try:
     from .local import *
 except ImportError:
